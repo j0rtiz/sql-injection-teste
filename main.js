66b2d6723cc5ad6c2f9bbc39ded531aa9f9f931e
Jairo Ortiz <jairo.ortiz@govbr.com.br>
2019-07-02 11:19:42 -0300

    a

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.post("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("TESTE");
 });
 
